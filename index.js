7e5521e38e8b8607bcbf9f990e192b6f5025c2fb
ChrisDeSimplon <christophe.delapierre.simplon@gmail.com>
2023-04-12 10:59:01 +0200

    server

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "path";
+const __dirname=path.resolve();
 //const express=require("express");
 
 const app=express();
+app.set("view engine","ejs");
+app.set("views","views");
+app.use(express.static(path.join(__dirname,"public")));
 app.get("/",(req,res)=>{
     res.send("hello world!!");
 });
+
 
 app.listen(8082,()=>{
     console.log('Server on port 8082 listenning');
