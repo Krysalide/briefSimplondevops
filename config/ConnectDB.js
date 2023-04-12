6cdc17cacd031e5cb06142ab8a2cd2de8b3de29a
ChrisDeSimplon <christophe.delapierre.simplon@gmail.com>
2023-04-12 15:14:59 +0200

    securité2

--- config/ConnectDB.js
+++ config/ConnectDB.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-//mongodb+srv://christophedelapierresimplon:<password>@clustersimplonchristoph.btp4k2s.mongodb.net/?retryWrites=true&w=majority
-//"mongodb+srv://christophedelapierresimplon:DFf0uvbc1bqGUwMj@clustersimplonchristoph.btp4k2s.mongodb.net/?retryWrites=true&w=majority"
+
 const   ConnectDB= async()=>{
 
     try {
